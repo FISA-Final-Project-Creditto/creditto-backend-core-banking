aa9c735fa040058b23599a375fd8c1e1f4db9c7a
Yang-Chaeyeon <yeon9915@naver.com>
2025-11-13 16:41:36 +0900

    refactor: 불필요한 송금 Repository 클래스 삭제

--- src/main/java/org/creditto/core_banking/domain/regularremittance/repository/MonthlyRegularRemittanceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.creditto.core_banking.domain.regularremittance.repository;
-
-import org.creditto.core_banking.domain.regularremittance.entity.MonthlyRegularRemittance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MonthlyRegularRemittanceRepository extends JpaRepository<MonthlyRegularRemittance, Long> {
-}
