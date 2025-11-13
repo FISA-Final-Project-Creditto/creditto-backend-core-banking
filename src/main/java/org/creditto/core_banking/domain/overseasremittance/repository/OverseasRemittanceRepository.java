7360dd9e8796b6ca57d0936942371dabb18f23b7
Yang-Chaeyeon <yeon9915@naver.com>
2025-11-13 16:31:21 +0900

    refactor: fetch전략 및 변수 타입 변경

--- src/main/java/org/creditto/core_banking/domain/overseasremittance/repository/OverseasRemittanceRepository.java
+++ src/main/java/org/creditto/core_banking/domain/overseasremittance/repository/OverseasRemittanceRepository.java
@@ -2,8 +2,16 @@ package org.creditto.core_banking.domain.overseasremittance.repository;
 
 import org.creditto.core_banking.domain.overseasremittance.entity.OverseasRemittance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface OverseasRemittanceRepository extends JpaRepository<OverseasRemittance,Long> {
+public interface OverseasRemittanceRepository extends JpaRepository<OverseasRemittance, Long> {
+
+    // FETCH JOIN으로 N+1문제 방지
+    @Query("SELECT r FROM OverseasRemittance r JOIN FETCH r.recipient JOIN FETCH r.account JOIN FETCH r.fee LEFT JOIN FETCH r.recur WHERE r.clientId = :clientId")
+    List<OverseasRemittance> findByClientIdWithDetails(@Param("clientId") String clientId);
 }
