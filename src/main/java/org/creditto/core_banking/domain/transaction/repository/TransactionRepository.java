d308dbe94cb947c98c52e8e23d89669ed5032272
kswdot <rlatjddms7a@naver.com>
2025-11-18 15:19:18 +0900

    fix: 코드 import 수정

--- src/main/java/org/creditto/core_banking/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/org/creditto/core_banking/domain/transaction/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package org.creditto.core_banking.domain.transaction.repository;
 
 import org.creditto.core_banking.domain.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
