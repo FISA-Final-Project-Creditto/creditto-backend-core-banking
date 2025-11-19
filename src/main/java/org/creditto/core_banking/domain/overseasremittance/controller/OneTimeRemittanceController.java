936b68c4b0a842a74bd94188b308da9ec9235f14
Jeyong <dlwpdyd201@naver.com>
2025-11-19 12:29:19 +0900

    refactor: Remittance Controller 응답객체 수정
    # Conflicts:
    #	src/main/java/org/creditto/core_banking/domain/account/controller/AccountController.java

--- src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/OneTimeRemittanceController.java
+++ src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/OneTimeRemittanceController.java
@@ -6,7 +6,14 @@ import org.creditto.core_banking.domain.overseasremittance.dto.OverseasRemittanc
 import org.creditto.core_banking.domain.overseasremittance.dto.OverseasRemittanceResponseDto;
 import org.creditto.core_banking.domain.overseasremittance.service.OneTimeRemittanceService;
 import org.creditto.core_banking.global.response.ApiResponseUtil;
+import org.creditto.core_banking.global.response.BaseResponse;
+import org.creditto.core_banking.global.response.ApiResponseUtil;
 import org.creditto.core_banking.global.response.SuccessCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,4 +40,5 @@ public class OneTimeRemittanceController {
     ) {
         return ApiResponseUtil.success(SuccessCode.OK, oneTimeRemittanceService.processRemittance(externalUserId, request));
     }
+
 }
