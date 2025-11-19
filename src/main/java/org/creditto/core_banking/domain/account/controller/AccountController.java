5b5066d2b310ca8144e2fbf5fb1ca105b1c079e5
kswdot <rlatjddms7a@naver.com>
2025-11-19 16:49:05 +0900

    refactor: Account API 수정

--- src/main/java/org/creditto/core_banking/domain/account/controller/AccountController.java
+++ src/main/java/org/creditto/core_banking/domain/account/controller/AccountController.java
@@ -2,6 +2,7 @@ package org.creditto.core_banking.domain.account.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.creditto.core_banking.domain.account.dto.AccountCreateReq;
+import org.creditto.core_banking.domain.account.dto.AccountRes;
 import org.creditto.core_banking.domain.account.service.AccountService;
 import org.creditto.core_banking.global.response.ApiResponseUtil;
 import org.creditto.core_banking.global.response.BaseResponse;
@@ -27,9 +28,15 @@ public class AccountController {
         return ApiResponseUtil.success(SuccessCode.CREATED, accountService.createAccount(request, externalUserId));
     }
 
-    @GetMapping("/{accountId}/balance")
+    @GetMapping("/{accountId}/account")
     public ResponseEntity<BaseResponse<?>> getAccountByAccountId(@PathVariable Long accountId) {
         return ApiResponseUtil.success(SuccessCode.OK, accountService.getAccountById(accountId));
+    }
+
+    @GetMapping("/{accountId}/balance")
+    public ResponseEntity<BaseResponse<?>> getAccountBalanceByAccountId(@PathVariable Long accountId) {
+        AccountRes accountRes = accountService.getAccountById(accountId);
+        return ApiResponseUtil.success(SuccessCode.OK, accountRes.balance());
     }
 
     @GetMapping("/{accountNo}")
