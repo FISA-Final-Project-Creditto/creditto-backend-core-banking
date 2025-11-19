47d2109aa23c4a3b8372fbc99b701e2af8ad51fa
Jeyong <dlwpdyd201@naver.com>
2025-11-19 14:27:25 +0900

    refactor: OneTimeRemittanceController 요청시 userId를 PathVariable로 받도록 수정

--- src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/OneTimeRemittanceController.java
+++ src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/OneTimeRemittanceController.java
@@ -6,13 +6,9 @@ import org.creditto.core_banking.domain.overseasremittance.dto.OverseasRemittanc
 import org.creditto.core_banking.domain.overseasremittance.dto.OverseasRemittanceResponseDto;
 import org.creditto.core_banking.domain.overseasremittance.service.OneTimeRemittanceService;
 import org.creditto.core_banking.global.response.ApiResponseUtil;
-import org.creditto.core_banking.global.response.BaseResponse;
 import org.creditto.core_banking.global.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 일회성 해외송금 요청을 처리하는 API 컨트롤러입니다.
@@ -21,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/core/remittance")
 public class OneTimeRemittanceController {
+
     private final OneTimeRemittanceService oneTimeRemittanceService;
 
     /**
@@ -29,8 +26,11 @@ public class OneTimeRemittanceController {
      * @param request 송금에 필요한 정보(고객 ID, 수취인 정보, 금액 등)를 담은 DTO
      * @return 처리 결과를 담은 응답 DTO ({@link OverseasRemittanceResponseDto})
      */
-    @PostMapping
-    public ResponseEntity<?> processRemittance(@Valid @RequestBody OverseasRemittanceRequestDto request) {
-        return ApiResponseUtil.success(SuccessCode.OK, oneTimeRemittanceService.processRemittance(request));
+    @PostMapping("/{externalUserId}")
+    public ResponseEntity<?> processRemittance(
+            @PathVariable String externalUserId,
+            @Valid @RequestBody OverseasRemittanceRequestDto request
+    ) {
+        return ApiResponseUtil.success(SuccessCode.OK, oneTimeRemittanceService.processRemittance(externalUserId, request));
     }
 }
