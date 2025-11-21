de5ee98a662e96c18bf293f35683d8cbf6f50508
wo-oaw <wo-oaw@naver.com>
2025-11-21 10:53:05 +0900

    chore: pull main

--- src/main/java/org/creditto/core_banking/domain/regularremittance/entity/RegularRemittance.java
+++ src/main/java/org/creditto/core_banking/domain/regularremittance/entity/RegularRemittance.java
@@ -9,8 +9,12 @@ import lombok.experimental.SuperBuilder;
 import org.creditto.core_banking.domain.account.entity.Account;
 import org.creditto.core_banking.domain.recipient.entity.Recipient;
 import org.creditto.core_banking.global.common.CurrencyCode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -19,27 +23,40 @@ import java.math.BigDecimal;
 @DiscriminatorColumn(name = "regrem_type")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class RegularRemittance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long regRemId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Account account;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     private Recipient recipient;
 
-    @Enumerated(EnumType.STRING)
     private CurrencyCode sendCurrency;
 
-    @Enumerated(EnumType.STRING)
     private CurrencyCode receivedCurrency;
 
     private BigDecimal sendAmount;
 
     @Enumerated(EnumType.STRING)
     private RegRemStatus regRemStatus;
+
+    // 엔티티 생성 시각 자동 저장
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
+    private LocalDateTime createdAt;
+
+    // 엔티티 수정 시각 자동 저장
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private LocalDateTime updatedAt;
+
+    public void updateRegRemStatus(RegRemStatus regRemStatus) {
+        this.regRemStatus = regRemStatus;
+    }
 
     public void updateDetails(
             Account account,
