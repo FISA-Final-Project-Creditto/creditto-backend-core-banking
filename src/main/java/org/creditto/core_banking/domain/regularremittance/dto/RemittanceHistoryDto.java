80d6d072952707b1ef8fad70bb9b59971899b9b9
wo-oaw <wo-oaw@naver.com>
2025-11-29 10:45:09 +0900

    feat: 정기 송금 내역 리스트 반환 API에 응답값 추가 (#88)

--- src/main/java/org/creditto/core_banking/domain/regularremittance/dto/RemittanceHistoryDto.java
+++ src/main/java/org/creditto/core_banking/domain/regularremittance/dto/RemittanceHistoryDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 public class RemittanceHistoryDto {
+    Long remittanceId;
     BigDecimal sendAmount;
     BigDecimal exchangeRate;
     LocalDate createdDate;
