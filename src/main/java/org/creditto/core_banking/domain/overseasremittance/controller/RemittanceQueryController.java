67d88d799dc05984e3c3d957aca28d81c1b211cb
Yang-Chaeyeon <51074379+Yang-Chaeyeon@users.noreply.github.com>
2025-11-30 16:23:09 +0900

    feat: 신용도 분석용 데이터 조회 api 구현 (#66)

--- src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/RemittanceQueryController.java
+++ src/main/java/org/creditto/core_banking/domain/overseasremittance/controller/RemittanceQueryController.java
@@ -2,6 +2,7 @@ package org.creditto.core_banking.domain.overseasremittance.controller;
 // 송금에 관하여 일회/정기 구분 없이 통합해서
 
 import lombok.RequiredArgsConstructor;
+import org.creditto.core_banking.domain.overseasremittance.dto.CreditAnalysisRes;
 import org.creditto.core_banking.domain.overseasremittance.dto.OverseasRemittanceResponseDto;
 import org.creditto.core_banking.domain.overseasremittance.service.RemittanceQueryService;
 import org.creditto.core_banking.global.response.ApiResponseUtil;
@@ -9,6 +10,7 @@ import org.creditto.core_banking.global.response.BaseResponse;
 import org.creditto.core_banking.global.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,16 @@ public class RemittanceQueryController {
     @GetMapping
     public ResponseEntity<BaseResponse<List<OverseasRemittanceResponseDto>>> getRemittanceList(@RequestParam Long userId) {
         return ApiResponseUtil.success(SuccessCode.OK, remittanceService.getRemittanceList(userId));
+    }
+
+    /**
+     * 특정 고객의 모든 송금 내역에 대한 신용도 분석용 데이터를 조회합니다.
+     *
+     * @param userId 조회할 고객의 ID
+     * @return 해당 고객의 모든 송금에 대한 신용 분석용 데이터 리스트 ({@link CreditAnalysisRes})
+     */
+    @GetMapping("/credit-analysis/{userId}")
+    public ResponseEntity<BaseResponse<List<CreditAnalysisRes>>> getCreditAnalysisData(@PathVariable Long userId) {
+        return ApiResponseUtil.success(SuccessCode.OK, remittanceService.getCreditAnalysisData(userId));
     }
 }
